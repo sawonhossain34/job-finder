d919d3afd600f5a5c144f5aa05e915222f603fa6
sawonhossain34 <sawonhossain3491@gmail.com>
2023-10-06 16:29:15 +0600

    footer and value some work

--- src/Components/FooterDiv/FooterDiv.jsx
+++ src/Components/FooterDiv/FooterDiv.jsx
@@ -1,9 +1,66 @@
+import { AiFillInstagram } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
+import { AiOutlineTwitter } from 'react-icons/ai';
 
 
 const FooterDiv = () => {
     return (
-        <div>
-            <h1>Footer div</h1>
+        <div className="footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 m-auto items-start justify-center">
+            <div>
+                <div className="logoDiv">
+                    <h1 className="logo text-[25px]  text-white pb-[1.5rem]"><strong>Job</strong>Search</h1>
+                </div>
+                <p className="text-white pb-[13px] opacity-70 leading-7">We always make our seekers and companies find the best job and employers find the best candidates</p>
+            </div>
+
+            <div className="grid">
+                <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+                    Company
+                </span>
+                <div className="grid gap-3">
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">About Us</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Features</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">News</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Faq</li>
+                </div>
+            </div>
+            <div className="grid">
+                <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+                    Resources
+                </span>
+                <div className="grid gap-3">
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Account</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Support Center</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Feedback</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Contact Us</li>
+                </div>
+            </div>
+            <div className="grid">
+                <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+                    Support
+                </span>
+                <div className="grid gap-3">
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Events</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Promo</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Req Demo</li>
+                    <li className="text-white opacity-[.7] hover:opacity-[1]">Careers</li>
+                </div>
+            </div>
+            <div className="grid">
+                <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+                    Contact Info
+                </span>
+                <div className="">
+                    <small className="text-white text-[15px]">
+                        sawonhossain3491@gmail.com
+                    </small>
+                    <div className="icons flex gap-4 py-4">
+                        <AiFillInstagram className='bg-white p-5 w-[36px] h-[36px] rounded-full icon text-black' />
+                        <BsFacebook className='bg-white p-5 w-[36px] h-[36px]  rounded-full icon text-blueColor' />
+                        <AiOutlineTwitter className='bg-white p-5 w-[36px] h-[36px]  rounded-full icon text-blueColor' />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/ValueDiv/ValueDiv.jsx
+++ src/Components/ValueDiv/ValueDiv.jsx
@@ -3,7 +3,7 @@ import simply from "../../assets/react.svg"
 const ValueDiv = () => {
     return (
         <div className="mb-[4rem] mt-[6rem]">
-            <h1 className="text-textColor text-[25px] py-[23rem] pb-[3rem] font-bold w-[400px] block">The value thats holds us true and to account
+            <h1 className="text-textColor text-[25px] py-[3rem] pb-[3rem] font-bold w-[400px] block">The value thats holds us true and to account
             </h1>
             <div className=" grid grid-cols-3 gap-[10rem] items-center">
                 <div className="singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]">
@@ -26,7 +26,7 @@ const ValueDiv = () => {
                         <span className="font-semibold text-textColor text-[18px]">Simplicity</span>
                     </div>
                     <p className="text-[15px] text-textColor opacity-[.7] font-semibold py-[1rem]">
-                        We believe in maketing better for everyone,even if just by a little bit. 
+                        We believe in maketing better for everyone,even if just by a little bit.
                     </p>
 
                 </div>
@@ -42,6 +42,13 @@ const ValueDiv = () => {
                     </p>
 
                 </div>
+            </div>
+            <div className=" bg-greyIsh  mt-8 flex justify-between  p-20 rounded-xl">
+                <div>
+                    <h1 className="text-blueColor font-bold text-4xl">Ready to switch a career</h1>
+                    <h1 className="text-black font-bold text-4xl">Lets get started!</h1>
+                </div>
+                <button className="border-[2px] rounded-[10px] py-[4px] px-[50px] text-2xl font-semibold text-blueColor hover:bg-white border-blueColor">Gets Started</button>
             </div>
         </div>
     );
